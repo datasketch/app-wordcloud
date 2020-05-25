bc001a8057fa73be5b087b0cb4931d39ac3b0338
brazadas <anaherand@hotmail.com>
2020-05-24 19:59:40 -0500

    internationalization

--- app.R
+++ app.R
@@ -2,16 +2,15 @@ library(shinypanels)
 library(parmesan)
 library(shinyinvoer)
 library(shi18ny)
+library(V8)
 library(dsmodules)
 library(tidyverse)
 library(wordcloud2)
 library(htmlwidgets)
 library(tm)
 library(readtext)
+library(webshot)
 
-# Internacionalización
-# Arreglar código
-# Falta el download selfcontained
 
 
 ui <- panelsPage(useShi18ny(),
@@ -30,14 +29,12 @@ ui <- panelsPage(useShi18ny(),
                        color = "chardonnay",
                        can_collapse = FALSE,
                        body = div(langSelectorInput("lang", position = "fixed"),
-                                  #wordcloud2Output("result"),
                                   uiOutput("result"),
-                                  shinypanels::modal(id = "test",
+                                  shinypanels::modal(id = "download",
                                                      title = ui_("download_plot"),
-                                                     # dsmodules::downloadImageUI("download_data_button", "Download HTML")),
-                                                     # dsmodules::downloadHtmlwidgetUI("download_data_button", "Download HTML"))),
                                                      uiOutput("modal"))),
-                       footer = shinypanels::modalButton(label = "Download plot", modal_id = "test")))
+                       footer = shinypanels::modalButton(label = "Download plot", modal_id = "download")))
+
 
 
 server <- function(input, output, session) {
@@ -47,7 +44,6 @@ server <- function(input, output, session) {
   observeEvent(lang(), {
     uiLangUpdate(input$shi18ny_ui_classes, lang())
   })  
-  
   
   output$text_input <- renderUI({
     choices <- c("sampleData", "pasted", "fileUpload", "url")
@@ -61,7 +57,7 @@ server <- function(input, output, session) {
   parmesan <- parmesan_load(path)
   parmesan_input <- parmesan_watch(input, parmesan)
   parmesan_alert(parmesan, env = environment())
-  parmesan_lang <- reactive({i_(parmesan, lang(), keys = c("label", "choices"))})
+  parmesan_lang <- reactive({i_(parmesan, lang(), keys = c("label", "choices", "text"))})
   output_parmesan("controls", 
                   parmesan = parmesan_lang,
                   input = input, 
@@ -70,12 +66,13 @@ server <- function(input, output, session) {
   
   output$modal <- renderUI({
     dw <- i_("download", lang())
-    downloadHtmlwidgetUI("download_data_button", paste(dw, "HTML"))
+    downloadImageUI("download_data_button", dw, formats = c("jpeg", "png", "pdf"))
+    # downloadHtmlwidgetUI("download_data_button", paste(dw, "PNG"))
   })
   
   labels <- reactive({
     list(sampleLabel = i_("sample_lb", lang()), 
-         sampleFiles = list("Montés" = "data/sampleData/nvtm"),
+         sampleFiles = list("Smithsonian -- Tigers" = "data/sampleData/nvtm"),
          pasteLabel = i_("paste", lang()), pasteValue = "", pastePlaceholder = i_("paste_pl", lang()), pasteRows = 5, 
          uploadLabel = i_("upload_lb", lang()), uploadButtonLabel = i_("upload_bt_lb", lang()), uploadPlaceholder = i_("upload_pl", lang()),
          urlLabel = i_("url_lb", lang()),
@@ -85,22 +82,34 @@ server <- function(input, output, session) {
                          "url" = "Se extraen los párrafos (el contenido de los HTML tags p) de la página web"))
   })
   
-  observeEvent(labels(),{
-  # observe({
-  # datasetInput <- eventReactive(labels(), {
-  datasetInput <<- do.call(callModule, c(textDocumentInput,
-                          # do.call(callModule, c(t0,
-                          "initial_data",
-                          labels()))
+  observeEvent(lang(), {
+    ch0 <- as.character(parmesan$words$inputs[[3]]$input_params$choices)
+    names(ch0) <- i_(ch0, lang())
+    ch1 <- as.character(parmesan$styles$inputs[[5]]$input_params$choices)
+    names(ch1) <- i_(ch1, lang())
+    ch2 <- as.character(parmesan$sizes$inputs[[1]]$input_params$choices)
+    names(ch2) <- i_(ch2, lang())
+    updateSelectInput(session, "words_language", choices = ch0, selected = input$words_language)
+    updateSelectInput(session, "background_color", choices = ch1, selected = input$background_color)
+    updateSelectInput(session, "shape", choices = ch2, selected = input$shape)
+  })
+  
+  td <- reactiveValues(text_uploaded = NULL)
+  
+  observe({
+    td$text_uploaded <- do.call(callModule, c(textDocumentInput,
+                                              "initial_data",
+                                              labels()))
   })
   
   output$data_preview <- renderUI({
-    req(datasetInput())
-    HTML(paste0("<div style = 'box-shadow: -3px 3px 5px 2px rgba(0, 0, 0, 0.06); padding: 12px 10px;'>", datasetInput(), "</div>"))
+    HTML(paste0("<div style = 'box-shadow: -3px 3px 5px 2px rgba(0, 0, 0, 0.06); padding: 12px 10px;'>", td$text_uploaded(), "</div>"))
   })
   
   tb <- reactive({
-    dt0 <- datasetInput()
+    dt0 <- td$text_uploaded()
+    dt0 <- gsub("<br/>", "", dt0)
+    
     if (!is.data.frame(dt0)) {
       dt0 <- gsub("\\.|,|;|¡|!|\\?|¿|-|:", "", dt0)
       dt0 <- strsplit(dt0, " ")[[1]]
@@ -110,7 +119,7 @@ server <- function(input, output, session) {
     if (input$stop_words) {
       dt1 <- setdiff(dt0$dt0, tm::stopwords(input$words_language))
       dt2 <- data.frame(pl = dt1) %>%
-        left_join(dt0, by = c("pl" = "dt0")) 
+        left_join(dt0, by = c("pl" = "dt0"))
       dt0 <- dt2
     }
     dt0 %>%
@@ -123,14 +132,19 @@ server <- function(input, output, session) {
   
   wd <- reactive({
     req(tb(), input$top_n)
-    
-    wordcloud2(data = tb()[1:input$top_n, ], 
+    thm <- input$theme
+    if (input$theme == "datasketch") {
+      input$top_n
+      thm <- rep(c("#3DB26F", "#FECA84", "#74D1F7", "#F75E64", "#8097A4", "#B70F7F", "#5D6AE9", "#53255E", "#BDCAD1"),
+                 ceiling(input$top_n / 9))
+    }
+    wordcloud2(data = tb()[1:input$top_n, ],
                size = input$size,
-               minSize = input$min_size,
+               # minSize = input$min_size,
                gridSize = input$grid_size,
                fontFamily = input$font_family,
                fontWeight = input$font_weight,
-               color = input$theme,
+               color = thm,
                backgroundColor = input$background_color,
                minRotation = pi * input$rotation[1],
                maxRotation = pi * input$rotation[2],
@@ -139,32 +153,43 @@ server <- function(input, output, session) {
                ellipticity = input$ellipticity,
                widgetsize = c(input$width, input$height)
     )
-  })  
+  })
   
   output$result <- renderUI({
-    session$sendCustomMessage("setButtonState", c("none", "download_data_button-downloadHtmlwidget"))
+    lapply(c("jpeg", "png", "pdf"), function(z) {
+      buttonId <- paste0("download_data_button-DownloadImg", z)
+      session$sendCustomMessage("setButtonState", c("none", buttonId))
+    })
     wordcloud2Output("wc", width = input$width, height = input$height)
   })
   
   output$wc <- renderWordcloud2({
-    session$sendCustomMessage("setButtonState", c("none", "download_data_button-downloadHtmlwidget"))
+    session$sendCustomMessage("setButtonState", c("none", "download_data_button-downloadImage"))
     req(wd())
-    # withProgress(message = 'Calculation in progress',
-    #              detail = 'This may take a while...', value = 0, {
-    #                for (i in 1:15) {
-    #                  incProgress(1/15)
-    #                  Sys.sleep(0.25)
-    #                }
-    #              })
-    # assign("wd", wd(), envir = globalenv())
     wd()
-  })    
+  })
   
+  lapply(c("jpeg", "png", "pdf"), function(z) {
+    buttonId <- paste0("download_data_button-DownloadImg", z)
+    
+    output[[paste0("download_data_button-DownloadImg", z)]] <- downloadHandler(
+      filename = function() {
+        session$sendCustomMessage("setButtonState", c("loading", buttonId))
+        paste0("wordcloud-", gsub(" ", "_", substr(as.POSIXct(Sys.time()), 1, 19)), ".", z)
+      },
+      content = function(file) {
+        owd <- setwd(tempdir())
+        on.exit(setwd(owd))
+        widget <- wd()
+        saveWidget(widget, file = "tmp.html", selfcontained = FALSE)
+        webshot::webshot("tmp.html", file, cliprect = c(0, 0, 905, 705), delay = 3.5)
+        session$sendCustomMessage('setButtonState', c('done', buttonId))
+      }
+    )
+  })
   
-  # descargas
-  callModule(downloadHtmlwidget, "download_data_button", widget = reactive(wd()), name = "wordcloud")
-  # callModule(downloadImage, "download_data_button", graph = wd(), lib = "highcharter", formats = "html")
 }
+
 
 
 shinyApp(ui, server)
